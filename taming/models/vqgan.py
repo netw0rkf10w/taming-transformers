3b4d7d42289885f58795fa612093fde7e9a467fa
D. Khuê Lê-Huu <huudienkhue.le@gmail.com>
2022-11-02 12:20:09 +0100

    Remove dependencies on PyTorch Lightning (for inference only)

--- taming/models/vqgan.py
+++ taming/models/vqgan.py
@@ -1,15 +1,28 @@
+import importlib
 import torch
 import torch.nn.functional as F
-import pytorch_lightning as pl
 
-from main import instantiate_from_config
+# import pytorch_lightning as pl
+#from main import instantiate_from_config
 
 from taming.modules.diffusionmodules.model import Encoder, Decoder
 from taming.modules.vqvae.quantize import VectorQuantizer2 as VectorQuantizer
 from taming.modules.vqvae.quantize import GumbelQuantize
 from taming.modules.vqvae.quantize import EMAVectorQuantizer
 
-class VQModel(pl.LightningModule):
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+class VQModel(torch.nn.Module):
     def __init__(self,
                  ddconfig,
                  lossconfig,
